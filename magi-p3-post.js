89f9f7c479b3660a2f02d290b11beff9cb0860ae
web-padawan <serguey.kulikov@gmail.com>
2018-08-27 13:06:28 +0300

    Remove fixme comments added by modulizer

--- magi-p3-post.js
+++ magi-p3-post.js
@@ -1,11 +1,21 @@
+const fixme = `
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;`
+
 module.exports = {
   files: require('./package.json').files,
   from: [
     "const $_documentContainer = document.createElement('template');",
-    "$_documentContainer.innerHTML = `"
+    "$_documentContainer.innerHTML = `",
+    fixme
   ],
   to: [
     "import { html } from '@polymer/polymer/lib/utils/html-tag.js';",
-    "const $_documentContainer = html`"
+    "const $_documentContainer = html`",
+    ""
   ]
 }
